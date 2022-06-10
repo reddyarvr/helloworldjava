e2b6612196a449c4c8e3885d2d26c4913e5568be
reddyarvr <106362887+reddyarvr@users.noreply.github.com>
2022-06-10 17:28:06 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 10th june!" -  05:23 PM IST);
+    System.out.println("Hello World 10th june!" -  05:28 PM IST);
     }
 }
