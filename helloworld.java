c4a98f42fb950200e71c0587405f0d4beaa05c4f
reddyarvr <106362887+reddyarvr@users.noreply.github.com>
2022-06-10 17:56:18 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 10th june! -  05:30 PM IST-webhook demo");
+    System.out.println("Hello World 10th june! -  06:00 PM IST-webhook demo1");
     }
 }
